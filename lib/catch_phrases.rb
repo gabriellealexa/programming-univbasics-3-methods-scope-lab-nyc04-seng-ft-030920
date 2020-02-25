2d0c708096eebd31b79a3d8089de7406c48d9d09
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-25 02:40:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,8 +2,9 @@ def mario
   phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
-end
-
-def toadstool
+  
+  def toadstool
   puts status
 end
+end
+
